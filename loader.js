824c753f4249bbe025ae6a32f6e3a1b9f6e42915
FND <fnd@localhost.localdomain>
2017-03-28 14:16:06 +0200

    restructured internals to avoid IIFE and improve minification
    this also gets rid of awkward sed preprocessing

--- loader.js
+++ loader.js
@@ -1,8 +1,8 @@
-var loadScripts = (function() {
-
-// NB: there doesn't seem to be a reliable way to prefetch scripts while
-//     executing them in order - cf. http://stackoverflow.com/a/17979532
-return function loadScripts(uris, callback) {
+// NB:
+// * not supported by IE 8 and below (see RequireJS source for details)
+// * there doesn't seem to be a reliable way to prefetch scripts while
+//   executing them in order - cf. http://stackoverflow.com/a/17979532
+function loadScripts(uris, callback) {
 	if(!uris.length) { // for API convenience
 		if(callback) {
 			callback();
@@ -12,28 +12,30 @@ return function loadScripts(uris, callback) {
 
 	uris = uris.slice();
 	var uri = uris.shift();
-	loadScript(uri, function() {
+	var load = loadScripts; // assists minification
+	load.loadScript(uri, function() {
 		if(uris.length) {
-			loadScripts(uris, callback);
+			load(uris, callback);
 		} else if(callback) {
 			callback();
 		}
 	}, function() {
 		throw new Error("failed to load script " + uri);
 	});
-};
+}
 
-// NB: not supported by IE 8 and below (see RequireJS source for details)
-function loadScript(uri, onLoad, onError) {
-	var node = document.createElement("script");
+loadScripts.loadScript = function(uri, onLoad, onError) {
+	// assist minification
+	var doc = document;
+	var ael = "addEventListener";
 
-	node.addEventListener("load", onLoad);
+	var node = doc.createElement("script");
+
+	node[ael]("load", onLoad);
 	if(onError) {
-		node.addEventListener("error", onError);
+		node[ael]("error", onError);
 	}
 
 	node.src = uri;
-	document.head.appendChild(node);
-}
-
-}());
+	doc.head.appendChild(node);
+};
